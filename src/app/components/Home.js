7d75cd065a04df516c3b195434a0a499042b80ad
riya.t <riya.t@directi.com>
2017-02-20 01:48:12 +0530

    minor change

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -11,6 +11,7 @@ export class Home extends React.Component {
             status: 1,
             newHomeLink: props.oldHomeLink
         };
+
         setTimeout(()=> {
             this.setState(
                 {status: 99}
