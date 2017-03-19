d4af71247676a5d4dc154d8106b1b1ffb011b8e4
riya.t <riya.t@directi.com>
2017-03-19 15:37:32 +0530

    reference example

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@
  */
 import React from "react";
 import {render} from "react-dom";
+import ReactDOM from "react-dom";
 import {Router,Route,browserHistory,IndexRoute} from "react-router"
 import {Home} from "./components/Home";
 import {User} from "./components/User";
@@ -10,17 +11,61 @@ import {Header} from "./components/Header";
 import {Root} from "./components/Root";
 class App extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {a: ''}
+    }
+
+    update() {
+        this.setState({
+            a: this.a.refs.input.value,
+            b: this.refs.b.value
+        })
+    }
+
     render() {
         return (
-            <Router history={browserHistory}>
-                <Route path={"/"} component={Root}>
-                    <IndexRoute component={Home}/>
-                    <Route path={"home"} component={Home}/>
-                    <Route path={"user/:id"} component={User}/>
-                </Route>
-                <Route path={"home-single"} component={Home}/>
-            </Router>
-        );
+            <div>
+                <Input
+                    ref={component=>this.a=component}
+                    update={this.update.bind(this)}
+                    />
+                {this.state.a}
+                <input
+                    ref="b"
+                    onChange={this.update.bind(this)}
+                    />
+                {this.state.b}
+                < hr/>
+            </div>);
+        /*   return (
+         <Router history={browserHistory}>
+         <Route path={"/"} component={Root}>
+         <IndexRoute component={Home}/>
+         <Route path={"home"} component={Home}/>
+         <Route path={"user/:id"} component={User}/>
+         </Route>
+         <Route path={"home-single"} component={Home}/>
+         </Router>
+         );*/
+    }
+
+}
+
+class Input extends React.Component {
+
+    render() {
+        return (<input onChange={this.props.update} ref="input"></input>);
+        /*   return (
+         <Router history={browserHistory}>
+         <Route path={"/"} component={Root}>
+         <IndexRoute component={Home}/>
+         <Route path={"home"} component={Home}/>
+         <Route path={"user/:id"} component={User}/>
+         </Route>
+         <Route path={"home-single"} component={Home}/>
+         </Router>
+         );*/
     }
 
 }
